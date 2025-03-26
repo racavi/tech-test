afff81f46a852f19ab4fe879c2f39b3dd2b44dda
racavi <63754467+racavi@users.noreply.github.com>
2025-03-26 13:43:22 +0100

    chore: add infrastructure layer price entity

---
+++ tech-test/src/main/java/com/rafacalvo/prices/infrastructure/persistence/PriceEntity.java
@@ -0,0 +1,30 @@
+package com.rafacalvo.prices.infrastructure.persistence;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table("PRICES")
+public class PriceEntity {
+
+    @Id
+    private Long id;
+    private Integer brandId;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private Integer priceList;
+    private Integer productId;
+    private Integer priority;
+    private BigDecimal price;
+    private String currency;
+
+}
